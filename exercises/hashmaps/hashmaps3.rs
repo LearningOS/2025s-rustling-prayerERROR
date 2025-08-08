442fdd88137e36c49a043033b6b7636e31e155bf
prayerERROR <qcsdbzzt3@gmail.com>
2025-08-08 10:40:00 +0000

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -34,11 +34,24 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        // Updata team1
+        if let Some(pos) = scores.get_mut(&team_1_name) {
+            pos.goals_scored += team_1_score;
+            pos.goals_conceded += team_2_score;
+        } else {
+            scores.insert(team_1_name.to_string(),
+                            Team {goals_scored:team_1_score,
+                            goals_conceded:team_2_score});
+        }
+        // Update team2
+        if let Some(pos) = scores.get_mut(&team_2_name) {
+            pos.goals_scored += team_2_score;
+            pos.goals_conceded += team_1_score;
+        } else {
+            scores.insert(team_2_name.to_string(),
+                            Team {goals_scored: team_2_score,
+                            goals_conceded: team_1_score});
+        }
     }
     scores
 }
